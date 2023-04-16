a9c5ccfe30d03348deb09c50a0c5f8e7d0d5d16d
ibqn <evgen.ibqn@gmail.com>
2023-04-16 22:04:03 +0200

    working on validators

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Median')
@@ -16,4 +19,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
